226ff11465bab546acc7f5187eb0caf2d5729027
kovals1 <sergey.kovalev@emc.com>
2018-05-28 23:20:46 +0300

    second commit

--- func_tools.py
+++ func_tools.py
@@ -2,6 +2,12 @@
 from functools import partial
 from inspect import getsource
 
+#(m a -> b) - > a - > b
+monada_s = lambda func, a: func(a) #(a - > b) -> a - > b
+monada_t = lambda func, a: func(*a) #(*a -> b) -> tupel(a)->b
+monada_d = lambda func, a: func(**a) #(**a -> b) -> dict(a)->b
+
+
 class ComposableError(Exception):
     pass
 
@@ -11,24 +17,42 @@ def wrap_arguments(*arg, **kw):
         return tuple(next(tmp, a) if a is Id else a for a in iter(arg))
     return wrapper
 
-class composable(object):
-    def __init__(self, func):
-        if type(func) is composable:
-            self._func = func._funcs
+class Composable(object):
+    def __init__(self, func, monada):
+        self._func_list = [(func, monada)]
+        self._monada = monada
+
+    @classmethod
+    def make_composable(cls, val, bind_type=monada_s):
+        if type(val) is cls:
+            return val
+        if type(val) is tuple:
+            return cls(wrap_arguments(*val), monada_t)
+        if type(val) is dict:
+            return cls(wrap_arguments(**val), monada_d)
+        if callable(val):
+            return cls(val, bind_type)
         else:
-            self._func = func
+            return cls(lambda *a: val, bind_type)
+
+    @classmethod
+    def _bind_composable(cls, comp_1, comp_2):
+        self = cls.__new__(cls)
+        func, _ = comp_2._func_list[0]
+        monada = comp_1._monada
+        self._func_list = comp_1._func_list + [(func, monada)] + comp_2._func_list[1:]
+        self._monada = comp_2._monada
+        return self
 
     # self >> fnc
     def __rshift__(self, other_fn):
-        if type(other_fn) is not composable:
-            other_fn = Composable(other_fn)._func
-        return composable(self._func + other_fn)
+        other_fn = self.make_composable(other_fn)
+        return self._bind_composable(self, other_fn)
 
     # self << fnc
     def __lshift__(self, other_fn):
-        if type(other_fn) is not composable:
-            other_fn = Composable(other_fn)._func
-        return composable(other_fn + self._func)
+        other_fn = self.make_composable(other_fn)
+        return other_fn >> self
 
     # fnc >> self
     __rrshift__ = __lshift__
@@ -37,47 +61,33 @@ class composable(object):
     __rlshift__ = __rshift__
 
     def __getattr__(self, name):
-        return composable(self._func + [lambda obj: getattr(obj, name)])
+        return self >> (lambda obj: getattr(obj, name))
 
     def __getitem__(self, name):
-        return composable(self._func + [lambda obj: obj[name]])
+        return self >> (lambda obj: obj[name])
 
     def __call__(self, *a):
-        a = self._func[0](*a)
-        return reduce(lambda res, func: func(res), self._func[1:], a)
+        func, _ = self._func_list[0]
+        res = func(*a)
+        return reduce(lambda res, (func, monada): monada(func, res), self._func_list[1:], res)
 
 
 def Arguments(*arg, **kw):
-    return composable([lambda x: wrap_arguments(*arg, **kw)(*x) if type(x) is tuple else wrap_arguments(*arg, **kw)(*x)])
-
-
-def Composable(func=lambda a: a):
-    if type(func) is tuple:
-        return Arguments(*func)
-    if type(func) is dict:
-        return Arguments(**func)
-    if type(func) is str:
-        return composable([lambda *a: func])
-    if type(func) is composable:
-        return composable(func._func)
-    if callable(func):
-        return composable([func])
+    return Composable.make_composable(arg)
 
 def log(*a):
     print("@@@ {}".format(a))
     return a
 
-Id = Composable()
-
-#uncurry = composable(rr, tuple)
-#curry = Id
-
-Log = Composable([log])
+Id = Composable.make_composable(lambda a: a)
+call = lambda *a: Composable.make_composable(lambda func: func(*a))
+composable = Composable.make_composable
+Log  = Composable.make_composable(log, monada_t)
+fmap = Composable.make_composable(map)
 Args = Arguments
-fmap = Composable(map)
 
 def main():
-    #assert(3 == Id(3))
+    assert(3 == Id(3))
     assert('test' == (Id << 'test')())
     assert(33 == ((Id << (lambda: {'d': 33}))['d'])())
     assert((1,3) == Args(Id, 3)(1))
